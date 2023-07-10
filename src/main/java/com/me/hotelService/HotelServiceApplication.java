3459618d8e4e2dfc5b4438138640d9e6ab6982d3
Megha867 <meghasamuelrampalli@gmail.com>
2023-07-10 09:22:56 +0100

    added staff controller
    new staff controller added

--- src/main/java/com/me/hotelService/HotelServiceApplication.java
+++ src/main/java/com/me/hotelService/HotelServiceApplication.java
@@ -2,6 +2,7 @@ package com.me.hotelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,12 @@ public class HotelServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelServiceApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate  restTemplate() {
+		return new RestTemplate();
 	}
 	
 
